3bfdcc0b223281cb2a57b416499d7f432b06f7d8
Naseer Momin <naseermomin555@gmail.com>
2024-09-09 19:45:36 +0530

    Create testing.py

---
+++ testing.py
@@ -0,0 +1,13 @@
+"""
+This module contains a simple hello function.
+"""
+
+def hello():
+    """
+    This function returns a greeting message.
+    """
+    return "Hello Dosto"
+
+
+if __name__ == "__main__":
+    hello()
